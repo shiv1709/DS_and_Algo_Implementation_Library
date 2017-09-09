704dd0bd3c5ac20e72958ab61d39917daea26041
SHIV PRATAP SINGH <shivpratap17singh@gmail.com>
2017-09-09 12:26:10 +0530

    Update Prims.cpp

--- Algorithms_Implementations/Graph_Algorithms/Greedy_Algorithms/Minimum_Spanning_Tree/Prims.cpp
+++ Algorithms_Implementations/Graph_Algorithms/Greedy_Algorithms/Minimum_Spanning_Tree/Prims.cpp
@@ -1,4 +1,5 @@
-//C++ Program for Linked List Representation of given Binary Tree(BT)
+//C++ Program for Implementation of Prims Algorithm 
+
 /*
 ***************************
 Author@Shiv Pratap Singh
