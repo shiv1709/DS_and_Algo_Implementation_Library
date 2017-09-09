94b30399d397c152bc7f4d3f9fa1fa2ba0977ae0
SHIV PRATAP SINGH <shivpratap17singh@gmail.com>
2017-09-09 12:16:28 +0530

    Update Breadth_First_Search(BFS).cpp

--- Algorithms_Implementations/Graph_Algorithms/Graph_Traversal/Breadth_First_Search(BFS).cpp
+++ Algorithms_Implementations/Graph_Algorithms/Graph_Traversal/Breadth_First_Search(BFS).cpp
@@ -1,9 +1,11 @@
-//C++ Program for Linked List Representation of given Binary Tree(BT)
+//C++ Program for Implementation of BFS(Breadth First Search) traversal of Graph
+
 /*
 ***************************
 Author@Shiv Pratap Singh
 ***************************
 */
+
 #include<bits/stdc++.h>
 using namespace std;
 class Graph
